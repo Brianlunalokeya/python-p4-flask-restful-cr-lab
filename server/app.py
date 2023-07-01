84f50af7825d8766dc701a86aa75ec2754780c1c
+ <+>
2023-07-01 07:10:12 +0300

    Done with Assignment

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -9,7 +9,7 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -17,11 +17,25 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        plant_dicts = [plant.as_dict() for plant in plants]
+        return jsonify(plant_dicts)
+
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(name=data['name'], image=data['image'], price=data['price'])
+        db.session.add(new_plant)
+        db.session.commit()
+        return jsonify(new_plant.as_dict())
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.get(id)
+        return jsonify(plant.as_dict())
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
